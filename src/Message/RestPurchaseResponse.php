810dce92bdc126ed857833e75b92c20f51f49981
petkokostov <petko.kostov@ampeco.global>
2022-06-10 17:34:33 +0300

    add: RestPurchaseResponse class

---
+++ src/Message/RestPurchaseResponse.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Omnipay\EpayNC\Message;
+
+class RestPurchaseResponse extends RestResponse
+{
+    /**
+     * @inheritdoc
+     */
+    public function isSuccessful()
+    {
+        return parent::isSuccessful() && 'SUCCESS' === $this->getData()['status'] && $this->checkOrderStatus();
+    }
+
+    public function getTransactionReference()
+    {
+        if (!$this->isSuccessful()) {
+            return null;
+        }
+        return $this->getData()['answer']['orderDetails']['orderId'];
+    }
+
+    private function checkOrderStatus()
+    {
+        $data = $this->getData();
+        if (!isset($data['answer'], $data['answer']['orderStatus'])) {
+            return false;
+        }
+
+        return 'PAID' === $data['answer']['orderStatus'];
+    }
+}
